c8723cd1fb5de2ee3e0a3127a2cdc4da0b6a2554
qsuelin <66089958+qsuelin@users.noreply.github.com>
2020-09-07 16:18:41 -0500

    bottom nav click all working

--- SaveDrivers_Instructor3/app/src/main/java/net/savedrivers/savedrivers_instructor3/ui/notifications/NotificationsFragment.java
+++ SaveDrivers_Instructor3/app/src/main/java/net/savedrivers/savedrivers_instructor3/ui/notifications/NotificationsFragment.java
@@ -13,6 +13,6 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        return inflater.inflate(R.layout.fragment_notifications,container, false);
     }
 }
