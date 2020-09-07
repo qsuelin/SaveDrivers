62ffc1f818658fe00c462b76601e5af4c1e31c96
qsuelin <66089958+qsuelin@users.noreply.github.com>
2020-09-07 16:07:54 -0500

    navigtion working except notification

--- SaveDrivers_Instructor3/app/src/main/java/net/savedrivers/savedrivers_instructor3/ui/instructions/InstructionsFragment.java
+++ SaveDrivers_Instructor3/app/src/main/java/net/savedrivers/savedrivers_instructor3/ui/instructions/InstructionsFragment.java
@@ -1,4 +1,19 @@
 package net.savedrivers.savedrivers_instructor3.ui.instructions;
 
-public class InstructionsFragment extends{
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import net.savedrivers.savedrivers_instructor3.R;
+
+public class InstructionsFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_instructions, container, false);
+    }
 }
